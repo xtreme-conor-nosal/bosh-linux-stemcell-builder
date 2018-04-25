b0d706a7f4d82d338ae3751523bac4bdfd5c0219
Danny Berger <dberger@pivotal.io>
2018-04-25 13:46:20 -0700

    wip
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>
    Co-authored-by: Difan Zhao <dzhao@pivotal.io>

---
+++ ci/ubuntu-xenial/initial_configure.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+
+pipeline="bosh:stemcells"
+args=""
+
+pipeline="$pipeline:$STEMCELL_OS-$STEMCELL_OS_VERSION"
+if [ -n "${INITIAL_STEMCELL_VERSION:-}" ]; then
+  initial_version=${INITIAL_STEMCELL_VERSION}
+else
+  initial_version="0.0.0"
+fi
+args="
+  -v initial_version=${initial_version}
+  -v stemcell_os=${STEMCELL_OS}
+  -v stemcell_os_version=${STEMCELL_OS_VERSION}
+"
+
+fly -t production set-pipeline \
+  -p "$pipeline" \
+  -c <( bosh interpolate $args "ci/$STEMCELL_OS-$STEMCELL_OS_VERSION/pipeline.yml" ) \
+  -l <( lpass show --note "concourse:production pipeline:os-images" ) \
+  -l <( lpass show --note "concourse:production pipeline:bosh:stemcells" ) \
+  -l <( lpass show --note "bats-concourse-pool:vsphere secrets" ) \
+  -l <( lpass show --note "tracker-bot-story-delivery" )
--- ci/xenial/configure.sh
+++ ci/xenial/configure.sh
@@ -3,6 +3,9 @@
 set -eu
 
 fly -t production set-pipeline \
-  -p bosh:xenial-stemcells -c ci/xenial/pipeline.yml \
-  -l <(lpass show --note "concourse:production pipeline:bosh:xenial-stemcells") \
-  -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
+  -p "bosh:xenial-stemcells" \
+  -c <( bosh interpolate ci/xenial/pipeline.yml ) \
+  -l <( lpass show --note "concourse:production pipeline:os-images" ) \
+  -l <( lpass show --note "concourse:production pipeline:bosh:stemcells" ) \
+  -l <( lpass show --note "bats-concourse-pool:vsphere secrets" ) \
+  -l <( lpass show --note "tracker-bot-story-delivery" )
