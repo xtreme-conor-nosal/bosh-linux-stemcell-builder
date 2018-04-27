176e46feab941aef8afe08454bd31353123dc137
Chris Dutra <cdutra@pivotal.io>
2018-04-27 15:57:49 -0700

    Automatically bump OS images
    - automatically build new OS images every 2 weeks, on Mondays
    - Remove OS_IMAGES.md
    - Consolidate OS images into Stemcells pipeline
    
    [#148866401](https://www.pivotaltracker.com/story/show/148866401)
    
    Signed-off-by: Josh Russett <jrussett@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,5 +4,6 @@ set -eu
 
 fly -t production set-pipeline \
   -p bosh:stemcells:3363.x -c ci/pipeline.yml \
-  -l <(lpass show --note "concourse:production pipeline:bosh:stemcells:3363.x") \
-  -l <(lpass show --note "bats-concourse-pool:vsphere secrets")
+  -l <(lpass show --notes "concourse:production pipeline:bosh:stemcells:3363.x") \
+  -l <(lpass show --notes "concourse:production pipeline:os-images") \
+  -l <(lpass show --notes "bats-concourse-pool:vsphere secrets")
